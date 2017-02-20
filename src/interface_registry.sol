49fb07a0b534ad682dcd25d0cf63cd32d0e052e4
Stephan Bothma <stephan@io.co.za>
2017-02-20 10:59:35 +0200

    Fix contract name

--- src/interface_registry.sol
+++ src/interface_registry.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.9;
 
-contract didmInterface {
+contract registry_interface {
   function verify(address did) constant returns (string ddo);
   function update(string _ddo) returns (bool updated);
   function revoke() returns (bool revoked);
